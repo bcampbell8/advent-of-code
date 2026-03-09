d0d5b4f12c2af7a3bd9105948c3af66afb760282
bcampbell8 <braeden.campbell8@gmail.com>
2026-03-09 18:32:03 +1100

    Added some simple notes to 4-1star.py on explaining the hash process in python with hashlib.

--- 2015/problem_4/4-1star.py
+++ 2015/problem_4/4-1star.py
@@ -3,6 +3,13 @@
 #follows my puzzle input (bgvyzdsv) that hashes to a string with atleast 5
 #leading zeroes.
 
+#Takeaways
+#To create a hash in python we can use the hashlib which comes as a default.
+#First you create a key for the hashing algorithm to use, then encode it as a
+#byte string (which can be done using 'b' before a string similar to f-strings)
+#then hash it using the hashing algorithm of your choice. This returns a hash
+#object which you then use hexdigest() to get the hexadecimal of the hash.
+
 import hashlib
 
 hashsample = "00000"
