a8cef7a5eba82ffeac0b9f3025ba683947546796
bcampbell8 <braeden.campbell8@gmail.com>
2026-03-16 16:06:24 +1100

    Fixed formatting and added some comments to solution

--- 2015/problem_7/7-1star.py
+++ 2015/problem_7/7-1star.py
@@ -1,9 +1,6 @@
-#Do i need to first find where some initial input is given and then look for
-#where it can be applied thereafter?
-
-#each line is one set of instructions.
-#the list provided is unsorted, but i think the wires are meant to be
-#calculated in alphabetical order.
+#The list of instructions is given in an unsorted list.
+#Applying each wire in alphabetical order is how wires are derived, with the
+#exception of wire 'a', which is solved last.
 import pdb
 instructions = {}
 with open("input.md", "r", encoding="utf-8") as f:
@@ -13,6 +10,11 @@ with open("input.md", "r", encoding="utf-8") as f:
 evaluated_ins = {}
 secondletter = None
 def evaluate_wires():
+    #Set two loops to evaluate each wire sequentially.
+    #The first loop represents iterations with wires that have more than one
+    #letter as a name e.g 'ab', in which it represents the first character
+    #The second loop represents the second character. It represents 'b' in the
+    #string 'ab', but it is also what is used for the single named wires.
     for h in range (0, 13):
         if h != 0:
             secondletter = 96 + h
@@ -22,17 +24,23 @@ def evaluate_wires():
             if (h == 0):
                 wire = instructions[f"{chr(i)}"].split("->")
             else:
-                wire = instructions[f"{chr(secondletter)}" + f"{chr(i)}"].split("->")
-            #breakpoint()
-            if len(wire[0].strip().split(" ")) == 1 and wire[0].strip().isnumeric():
+                wire = instructions[f"{chr(secondletter)}" + \
+                    f"{chr(i)}"].split("->")
+            #This checks for if the instruction is a purely numeric signal
+            #provided to a wire.
+            if len(wire[0].strip().split(" ")) == 1 and \
+                wire[0].strip().isnumeric():
                 evaluated_ins[f"{wire[1].strip("\n").strip()}"] = int(wire[0])
             else:
                 wire_words = wire[0].split(" ")
+                #This checks for the exceptional case of if an instruction is
+                #NOT, which only has two words that compose the instruction.
                 if wire_words[0].isupper() == True:
-                    evaluated_ins[f"{wire[1].strip("\n").strip()}"] = ~evaluated_ins[wire_words[1].strip()]
+                    evaluated_ins[f"{wire[1].strip("\n").strip()}"] = \
+                        ~evaluated_ins[wire_words[1].strip()]
                 else:
-                    #word1
-                    #word3
+                    #Format words, removing white space and then evaluate
+                    #expression based on bitwise operator.
                     if wire_words[0].strip().isalpha():
                         word1 = evaluated_ins[f"{wire_words[0].strip()}"]
                     else:
@@ -43,13 +51,17 @@ def evaluate_wires():
                         word3 = int(wire_words[2].strip())
                     match wire_words[1].strip():
                         case 'AND':
-                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = word1 & word3
+                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = \
+                                word1 & word3
                         case 'OR':
-                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = word1 | word3
+                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = \
+                                word1 | word3
                         case 'LSHIFT':
-                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = word1 << word3
+                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = \
+                                word1 << word3
                         case 'RSHIFT':
-                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = word1 >> word3
+                            evaluated_ins[f"{wire[1].strip().strip("\n")}"] = \
+                                word1 >> word3
 evaluate_wires()
 print(f"Wire a receives a signal of: {evaluated_ins['lx']}")
 
